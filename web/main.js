9311dbdaf250707cefcec8c89dc2db9f86ca12b4
tohyeeb <tohyeebn@gmail.com>
2023-12-08 23:43:22 +0000

    Trading AI page commit.

--- web/main.js
+++ web/main.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     $('.text').textillate({
-        loop:true,
-        sync:true,
-        in:{
+        loop: true,
+        sync: true,
+        in: {
             effect: "bounceIn",
         },
-        out:{
+        out: {
             effect: "bounceOut",
         },
     });
+    // Trading_ai-html
+    // JavaScript function to update button text while preserving the caret icon
+    window.updateButtonText = function(text) {
+        var button = document.getElementById('dropdownMenuButton1');
+        button.innerHTML = text + ' <i class="bi bi-caret-down-fill"></i>';
+    };
 
 });
